4f0866d9cceae1f359eb322b0fb1de7de399f520
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2025-01-02 23:43:10 -0300

    update js (#3)
    - core.js to scroll.js
    - scroll function
    - add bg_moving.js

---
+++ js/scrooll.js
@@ -0,0 +1,37 @@
+// init variables
+let indexPage = 0
+
+let divs = [
+  document.getElementById("amazonia"),
+  document.getElementById("caatinga"),
+  document.getElementById("cerrado"),
+  document.getElementById("pampa"),
+  document.getElementById("atlantica"),
+  document.getElementById("pantanal")
+]
+
+
+function reloadDivIndex() {
+  divs[indexPage].scrollIntoView({behavior: "smooth"})
+}
+
+function reloadLet() {
+  if (indexPage < 0) {
+    indexPage = 0
+  } 
+  if (indexPage > 5) {
+    indexPage = 5
+  }
+  console.log(indexPage)
+}
+
+// evente of wheel
+document.addEventListener("wheel", (Event) => {
+  if (Event.deltaY > 0) {
+    indexPage ++
+  } else {
+    indexPage --
+  }
+  reloadLet()
+  reloadDivIndex()
+})
